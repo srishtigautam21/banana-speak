bd2527811f3743ff4d35d312ec993fd0897d1048
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2021-12-03 12:11:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("script from another file");
+// console.assert("enter yes or no");
+var userAnswer=prompt("enter yes or no");
+var welcomemsg="scripting works " + userAnswer;
+alert(welcomemsg);
