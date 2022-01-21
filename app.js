e83fdcebd5bd34e36fb4cd98fb46f3f162a8f627
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-01-21 12:47:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
-var serverURL="https://api.funtranslations.com/translate/minion.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text)
 {
